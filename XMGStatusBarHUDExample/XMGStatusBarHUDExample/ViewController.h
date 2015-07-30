b97e3075893ef8ba47371f961e404869f74b04ed
“why” <why@163.com>
2015-07-30 15:15:00 +0800

    初始化框架
    1.显示成功信息
    2.显示失败信息
    3.显示加载信息
    4.隐藏HUD

---
+++ XMGStatusBarHUDExample/XMGStatusBarHUDExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMGStatusBarHUDExample
+//
+//  Created by xiaomage on 15/7/30.
+//  Copyright (c) 2015年 小码哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XMGStatusBarHUDExample/XMGStatusBarHUDExample/ViewController.m
@@ -0,0 +1,42 @@
+//
+//  ViewController.m
+//  XMGStatusBarHUDExample
+//
+//  Created by xiaomage on 15/7/30.
+//  Copyright (c) 2015年 小码哥. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "XMGStatusBarHUD.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)message:(id)sender {
+    [XMGStatusBarHUD showMessage:@"没有什么事!!!!"];
+}
+
+- (IBAction)hide:(id)sender {
+    [XMGStatusBarHUD hide];
+}
+
+- (IBAction)loading:(id)sender {
+    [XMGStatusBarHUD showLoading:@"正在加载中..."];
+}
+
+- (IBAction)error:(id)sender {
+    [XMGStatusBarHUD showError:@"加载失败!"];
+}
+
+- (IBAction)success:(id)sender {
+    [XMGStatusBarHUD showSuccess:@"加载成功!"];
+}
+@end
